698e49723f51eb690310de32dcb66cba6f8b489b
peggylicious <p.asq@live.com>
2018-12-04 16:35:59 +0100

    Update: Show and hide ticks when value is selected

--- Loop 1 - Step 1/assets/script.js
+++ Loop 1 - Step 1/assets/script.js
@@ -5,25 +5,37 @@
 * @param {string} defaultValue - The value that displays everytime browser refreshes
 * @param {string} inputVal - The selected value from list of options
 * @param {string} optBlock - The container for list of options
+* @param {string} checkmark - Checks a selected value
 * @returns {string} Selected option
 */
 
-let inputVal, defaultValue, optBlock, i;
-inputVal = document.querySelectorAll('.options p');
-defaultValue = document.querySelector('.default p');
-optBlock = document.getElementsByClassName('options')[0];
+var inputVal, defaultValue, optBlock, checkmark;
+    inputVal = document.querySelectorAll('.options p');
+    defaultValue = document.querySelector('.default p');
+    optBlock = document.getElementsByClassName('options')[0];
+    checkmark = document.getElementsByClassName('tick');
 
 // Displays default value when browser refreshes
+//  Hides all checkmarks when browser loads
 window.onload = function(){
-    optBlock.style.display = 'none';
+    for (let x = 0; x < inputVal.length; x++){
+        checkmark[x].style.visibility = 'hidden';
+    }
 }
 
 // Assigns click eventlistener to list of options
 // Hides Option block when value is selected
-for (i = 0; i < inputVal.length; i++){
+for (let i = 0; i < inputVal.length; i++){
     inputVal[i].addEventListener('click', function(e){
-        defaultValue.innerHTML = e.target.innerHTML;
+        defaultValue.innerHTML = this.innerHTML;
         optBlock.style.display = 'none';
+        checkmark[i].style.visibility = 'visible';
+        for (let a = 0; a < inputVal.length; a++){
+            if (inputVal[a] == event.target){
+                continue;
+            }
+            checkmark[a].style.visibility = 'hidden';
+        }
     })
 }
 
@@ -31,4 +43,3 @@ for (i = 0; i < inputVal.length; i++){
 defaultValue.addEventListener('click', function(e){
     optBlock.style.display = 'block';
 })
-
