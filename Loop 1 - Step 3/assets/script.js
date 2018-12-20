e372527dfd1cb0d9fa57e49b0096f91d64aaf96e
peggylicious <p.asq@live.com>
2018-12-20 16:12:38 +0100

    Update: comment out .js style

--- Loop 1 - Step 3/assets/script.js
+++ Loop 1 - Step 3/assets/script.js
@@ -3,9 +3,9 @@ document.addEventListener('click', function(event){
     for(var i = 0; i < switchA.length; i++){
         if(event.target == switchA[i]){
             // console.log(event.target);
-            if(switchA[i].hasAttribute('transform')){
-                console.log("has");
-            }
+            // if(switchA[i].hasAttribute('transform')){
+            //     console.log("has");
+            // }
             console.log(switchA[i]);
             switchA[i].classList.add('move_back');
             // break;
